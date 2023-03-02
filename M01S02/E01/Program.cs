7e88364bb1c77df8d700712662fcb4ccfc135988
josicaroline <joseanecaroline@hotmail.com>
2023-03-02 15:01:56 -0300

    initial commit

---
+++ M01S02/E01/Program.cs
@@ -0,0 +1,15 @@
+﻿
+//1 - Qual a descrição de um paradigma de POO ?
+//R: é um paradigma de programação baseado no conceito de "objetos";
+//um modelo de análise que baseia-se na aproximação entre o mundo real e o mundo virtual pela criação
+//e interação entre objetos, atributos, códigos e métodos.
+
+//2 - Quais os quatro pilares de POO ?
+//R: Os quatro pilares consistem em: herança, polimorfismo, encapsulamento e abstração.
+
+//3 - Cite uma estrutura de classe conforme a aula 1 ?
+//R:  Por exemplo uma Aluno tem os atributos nome, endereço, telefone e idade; 
+//E os metodos Registrar, matricular, cadastrar, estudar;
+//Esse exemplos seria uma estruturas de uma classe.
+
+// cifewhfewifhwei
